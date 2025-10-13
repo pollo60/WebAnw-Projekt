c023c02223457028847bc2952f57188b412b93b4
Altay Hennig <server.zehntel0s@icloud.com>
2025-10-13 15:09:08 +0200

    springboot test

---
+++ Altaystest/src/main/java/edu/fra/uas/Altaystest/AltaystestApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.Altaystest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltaystestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltaystestApplication.class, args);
+	}
+
+}
