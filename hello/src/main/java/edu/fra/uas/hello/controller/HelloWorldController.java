37e79dfad65c0a9e060bf2c78333c596213319d1
bulliGIT <sarahbu04@gmail.com>
2025-10-13 16:13:32 +0200

    yuhu

---
+++ hello/src/main/java/edu/fra/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package edu.fra.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.blind.annotation.RequestMapping;
+import org.springframework.web.blind.annotation.RequestMethod;
+import org.springframework.web.blind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+}
