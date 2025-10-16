2ad13a965ed32483dd2c4459a4be111e39326a36
Eisi <lea.eiswirth@web.de>
2025-10-16 12:28:11 +0200

    ersterCommit

---
+++ leaeiswirth/src/main/java/edu/fra/uas/leaeiswirth/LeaeiswirthApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.leaeiswirth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaeiswirthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaeiswirthApplication.class, args);
+	}
+
+}
---
+++ leaeiswirth/src/test/java/edu/fra/uas/leaeiswirth/LeaeiswirthApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.leaeiswirth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaeiswirthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
