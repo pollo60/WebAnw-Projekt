1ccc01160ef976644d1edc9def30af9896ae8974
MarcHrt <macimueller+gitHub@gmail.com>
2025-10-17 14:22:04 +0200

    try2

---
+++ marcel/src/main/java/edu/fra/uas/marcel/MarcelApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.marcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcelApplication.class, args);
+	}
+
+}
---
+++ marcel/src/test/java/edu/fra/uas/marcel/MarcelApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.marcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
