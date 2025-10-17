ca013aa68e19e588cd1c54bd025d3d855a350a34
JassineM <yassine.mameche@stud.fra-uas.de>
2025-10-17 13:50:19 +0200

    Hello Spring

---
+++ Jassine/src/main/java/edu/fra/uas/Jassine/JassineApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.Jassine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JassineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JassineApplication.class, args);
+	}
+
+}
---
+++ Jassine/src/test/java/edu/fra/uas/Jassine/JassineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.Jassine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JassineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
