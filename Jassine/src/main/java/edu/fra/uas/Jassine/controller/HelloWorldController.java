a1ae5f958255368029949bf05973b904188d3e72
JassineM <yassine.mameche@stud.fra-uas.de>
2025-10-17 17:08:51 +0200

    1.Instalation und erste Anwendung abgeschlossen!

--- Jassine/src/main/java/edu/fra/uas/Jassine/controller/HelloWorldController.java
+++ Jassine/src/main/java/edu/fra/uas/Jassine/controller/HelloWorldController.java
@@ -1,9 +1,8 @@
 package edu.fra.uas.Jassine.controller;
-import org.springframework.stererotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class HelloWorldController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
