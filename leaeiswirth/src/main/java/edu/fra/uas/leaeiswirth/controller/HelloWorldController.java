a9710d05844da095c4c3f5cc0f44f80b6d055922
Eisi <lea.eiswirth@web.de>
2025-10-20 11:46:03 +0200

    ControllerTest

---
+++ leaeiswirth/src/main/java/edu/fra/uas/leaeiswirth/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package edu.fra.uas.leaeiswirth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value="path", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld(){
+        return "<h1>Hello World Spring Boot! </h1>";
+
+    }
+    
+}
